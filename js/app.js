2b9587de67bdc94b2e8dcd53544617399b1f7512
Chantal <cflzr3@mail.missouri.edu>
2016-05-15 16:16:02 -0400

    adding everything

---
+++ js/app.js
@@ -0,0 +1,50 @@
+//app.js
+//require('angular-route');
+var app = angular.module('personalSite', ['ui.bootstrap', 'ngRoute']);
+//angular.module('myApp', [require('angular-route')]);
+app.controller('MainCtrl', function($scope) {
+    $scope.name = 'World';
+
+});
+app.config(function($routeProvider){
+  $routeProvider
+          .when('/aboutMe',{
+                templateUrl: 'templates/aboutMe.html',
+                controller: 'aboutCtrl'
+          })
+          .when('/education',{
+                templateUrl: 'templates/education.html',
+                controller: 'MainCtrl'
+              })
+          .when('/experience',{
+                    templateUrl: 'templates/experience.html',
+                    controller: 'MainCtrl'
+                  })
+          .when('/', {
+            templateUrl:'templates/home.html',
+            controller: 'MainCtrl'
+          })
+          .when('/home', {
+            templateUrl:'templates/home.html',
+            controller: 'MainCtrl'
+          })
+          .otherwise({
+        redirectTo: '/home'
+      });
+});
+app.controller('aboutCtrl', function($scope){
+
+
+
+});
+app.controller('educationCtrl', function($scope){
+
+
+
+});
+
+app.controller('experienceCtrl', function($scope){
+
+
+
+});
