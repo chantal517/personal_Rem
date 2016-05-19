70fce8432f2a236bf4d99253feac7354c975b3fc
chantal517 <Chantal@Chantals-MBP.hitronhub.home>
2016-05-19 16:50:18 -0500

    fixed typed text

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $( document ).ready(function() {
 
        $(".element").typed({
-           strings: ["Tech Enthusiast", "Student", "Software Engineer"],
+           strings: ["Tech Enthusiast", "Student", "Software Engineer", "Tech Enthusiast + Student + Software Engineer"],
            typeSpeed: 0,
            backDelay: 500,
            cursorChar: "",
            backSpeed:2
 
        });
+
+      // $(".element").html("Tech Enthusiast");
    });
 
 $(".wallpaper").css("min-height", $(window).height() );
