49c1febf78678eaa2129f38b6973b6301247d7b1
chantal517 <Chantal@mwc-106180.dhcp.missouri.edu>
2016-05-19 11:53:07 -0500

    Changed covere page with word typing and overall styling

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+       $(".element").typed({
+           strings: ["Tech Enthusiast", "Student", "Software Engineer"],
+           typeSpeed: 0,
+           backDelay: 500,
+           cursorChar: "",
+           backSpeed:2
+
+       });
+   });
+
+$(".wallpaper").css("min-height", $(window).height() );
