c446e7cabf86fe02fc462046b9a1e082981bb3b2
chantal517 <Chantal@Chantals-MacBook-Pro.local>
2016-09-16 17:27:05 -0500

    updated icons and added about me

--- js/main.js
+++ js/main.js
@@ -10,6 +10,25 @@ $( document ).ready(function() {
        });
 
       // $(".element").html("Tech Enthusiast");
+
+      $("#octo2").click(function() {
+          $('html,body').animate({
+              scrollTop: $(".aboutme").offset().top},
+              'slow');
+      });
+      $("#octo").mouseover(function() { $("#gi").css('visibility','visible'); });
+      $("#octo").mouseout(function() { $("#gi").css('visibility','hidden'); });
+
+      $("#octo3").mouseover(function() { $("#re").css('visibility','visible'); });
+      $("#octo3").mouseout(function() { $("#re").css('visibility','hidden'); });
+
+      $("#linked").mouseover(function() { $("#li").css('visibility','visible'); });
+      $("#linked").mouseout(function() { $("#li").css('visibility','hidden'); });
+
+      $("#octo2").mouseover(function() { $("#ab").css('visibility','visible'); });
+      $("#octo2").mouseout(function() { $("#ab").css('visibility','hidden'); });
+      $("#octo2").mouseover(function() { $("#octo2").css('color','#2db1d2'); });
+        $("#octo2").mouseout(function() { $("#octo2").css('color','white'); });
    });
 
 $(".wallpaper").css("min-height", $(window).height() );
